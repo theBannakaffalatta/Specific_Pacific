455dcbea59f692705a5edefaef258eae1f844376
theBannakaffalatta <karelius.steen@@live.no>
2018-02-12 22:16:33 +0200

    nhbklu

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float HorizontalSpeed;
+    public float VerticalSpeed;
+    public float jumpForce;
+
+    public float WalkingSpeed;
+    public float RunningSpeed;
+    public float CrouchingSpeed;
+
+
+
+    private Rigidbody rb;
+    public bool grounded;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        grounded = false;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        grounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        grounded = false;
+    }
+
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * HorizontalSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * VerticalSpeed;
+
+        transform.Translate(x, 0, z);
+
+        if (Input.GetKey("left shift"))
+        {
+            VerticalSpeed = RunningSpeed;
+        }
+
+        else if (Input.GetKey("left ctrl"))
+        {
+            HorizontalSpeed = CrouchingSpeed;
+            VerticalSpeed = CrouchingSpeed;
+
+        }
+
+        else
+        {
+            HorizontalSpeed = WalkingSpeed;
+            VerticalSpeed = WalkingSpeed;
+        }
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+            grounded = false;
+            Debug.Log("Jump");
+        }
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public static AudioClip s1, s2;
+    static AudioSource audioSrc;
+
+    // Use this for initialization
+    void Start()
+    {
+        s1 = Resources.Load<AudioClip>("Sound1");
+        s2 = Resources.Load<AudioClip>("Sound2");
+
+        audioSrc = GetComponent<AudioSource>();
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public static void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+            case "Sound1":
+                audioSrc.PlayOneShot(s1);
+                break;
+
+            case "Sound2":
+                audioSrc.PlayOneShot(s2);
+                break;
+        }
+    }
+}
